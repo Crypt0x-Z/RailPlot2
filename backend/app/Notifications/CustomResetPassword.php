99980ef37ffd31515d96c8b3f85959c624027187
KortelyesiBalint <77300580+KortelyesiBalint@users.noreply.github.com>
2025-04-28 05:15:44 +0200

    email, but reset password doesnt have css

---
+++ backend/app/Notifications/CustomResetPassword.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Auth\Notifications\ResetPassword;
+
+class CustomResetPassword extends ResetPassword
+{
+    public function toMail($notifiable)
+    {
+        $url = url(route('password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+
+        return (new MailMessage)
+            ->subject('Reset Your Password')
+            ->line('Click the button below to reset your password:')
+            ->action('Reset Password', $url)
+            ->line('If you did not request a password reset, no further action is required.');
+    }
+}
